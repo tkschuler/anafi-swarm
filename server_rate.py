eb50370431c19de6c176ccda038488627fa8c5ec
Tristan Schuler <tkschuler@yahoo.com>
2019-12-02 22:47:23 -0700

    added WP checking

--- server_rate.py
+++ server_rate.py
@@ -14,17 +14,45 @@ class Server:
         self.host = host
         self.server = None
         self.ON = True
+        self.waypoints = [1,2,3,4,5]
+        self.curWP = self.waypoints[0]
+        self.curWPindex = 0
+
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
 
     def newClient(self,clientsocket, addr):
+        #This listens for the incomming messages that client_rate.py is listening to ROS for
         while True:
             msg = clientsocket.recv(4096)
             print "data received:", msg
+            pose = msg.split(',')
             clientsocket.send("ACK!")
+
+            self.x = float(pose[0])
+            self.y = float(pose[1])
+            self.z = float(pose[2])
+            self.roll = float(pose[3])
+            self.pitch = float(pose[4])
+            self.yaw = float(pose[5])
 
         clientsocket.close()
 
-    def connect(self):
+    def checkWP(self):
+        #This is a 1D exaple for checking if the drone has made it to a current waypoint
+        while True:
+            if self.x == self.curWP:
+                print colored("MADE IT TO WP!", "green")
+                #send new drone control command
+                self.curWPindex +=1
+                self.curWP = self.waypoints[self.curWPindex]
 
+
+    def connect(self):
         self.s = socket.socket()
         print 'Server started!'
         print 'Waiting for clients...'
@@ -40,7 +68,10 @@ class Server:
                 print colored(('Got connection from' + str(addr)),"magenta")
                 self.server = Thread(target=self.newClient, args=((c,addr)))
                 self.server.daemon = True
+                self.wpcheck = Thread(target=self.checkWP)
+                self.wpcheck.daemon = True
                 self.server.start()
+                self.wpcheck.start()
             except:
                 print colored("Socket Error.", "red")
                 sys.exit()
--- vicontalker.py
+++ vicontalker.py
@@ -9,14 +9,17 @@ def talker():
     pub = rospy.Publisher("vicon/vicon/anafi_1", TwistStamped, queue_size=10)
     rospy.init_node('anafi_1', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+    i = .05
     while not rospy.is_shutdown():
         data = TwistStamped()
-        data.twist.linear.x = random()
+        data.twist.linear.x = i
         data.twist.linear.y = random()
         data.twist.linear.z = random()
         rospy.loginfo(data)
         pub.publish(data)
         rate.sleep()
+
+        i+= .025
 
 if __name__ == '__main__':
     seed(1)
